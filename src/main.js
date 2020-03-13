e4f62520f376d57674101469a89c6e2995b87bb7
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2020-03-12 20:06:36 -0600

    electron app example

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile(path.join(__dirname, '/index.html'));
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    })
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
